eb033b97469840ade192761e576a04fb2ce1a1d8
nightsky <1048418581@qq.com>
2026-08-25 01:07:34 +0800

    finish lab7 lock

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,22 +21,37 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; ++i)
+    initlock(&kmem[i].lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
+  push_off();
+  int id = cpuid();
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
+  acquire(&kmem[id].lock);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+  {
+    struct run *r;
+    void *pa = p;
+    if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+      panic("kfree");
+    memset(pa, 1, PGSIZE); // Fill with junk to catch dangling refs.
+    r = (struct run*)pa;
+    r->next = kmem[id].freelist;
+    kmem[id].freelist = r;
+  }
+  release(&kmem[id].lock);
+  pop_off();
 }
 
 // Free the page of physical memory pointed at by pa,
@@ -46,6 +61,9 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  push_off();
+  int id = cpuid();
+
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -56,10 +74,12 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +88,25 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
+  push_off();
+  int id = cpuid();
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  for(int i = 0; i < NCPU; ++i)
+  {
+    int victim = (id + i) % NCPU;
+    acquire(&kmem[victim].lock);
+    r = kmem[victim].freelist;
+    if(r)
+      kmem[victim].freelist = r->next;
+    release(&kmem[victim].lock);
+    if(r)
+    {
+      memset((char*)r, 5, PGSIZE); // fill with junk
+      break;
+    }
+  }
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+  pop_off();
   return (void*)r;
 }
--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -16,5 +16,8 @@ struct spinlock {
 struct rwspinlock {
   // Replace this with your implementation.
   struct spinlock l;
+  int readers;
+  int writer;
+  int waiting_writers;
 };
 #endif
